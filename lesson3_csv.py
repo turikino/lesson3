bc593084943981e116ff459df37d9e39cb595857
turikino <turikino84@gmail.com>
2019-03-06 00:32:42 +0300

    Добавил скрипт по работе с csv.

---
+++ lesson3_csv.py
@@ -0,0 +1,16 @@
+import csv
+
+
+user_list = [
+        {'name': 'Маша', 'age': 25, 'job': 'Scientist'},
+        {'name': 'Вася', 'age': 8, 'job': 'Programmer'},
+        {'name': 'Эдуард', 'age': 48, 'job': 'Big boss'},
+    ]
+
+with open('export.csv', 'w', encoding='UTF-8', newline='') as f:
+    fields = ['name', 'age', 'job']
+    writer = csv.DictWriter(f, fields, delimiter=';')
+    writer.writeheader()
+    for user in user_list:
+        writer.writerow(user)
+
